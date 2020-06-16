20ff2fe6223284515c6bcec0761707de1da6a47b
Mathias Blanckaert <mathias.blanckaert@net-it.be>
2020-06-16 11:29:27 +0200

    temp

--- Function1.cs
+++ Function1.cs
@@ -1,10 +1,24 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.Text.Json;
+using System.Xml;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Azure.WebJobs.ServiceBus;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.PowerPlatform.Cds.Client;
 using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
 
-namespace FunctionApp3
+namespace PortalSynchronization
 {
     [ServiceBusAccount("ServiceBusConnectionString")]
     public class Outgoing
@@ -34,16 +48,11 @@ namespace FunctionApp3
         }
 
         [FunctionName("accounts")]
-        public void Accounts([ServiceBusTrigger("%TopicName%", "%accountSubscriptionName%")]byte[] body,
+        public void Countries([ServiceBusTrigger("dumpqueue")]byte[] body,
             string contentType,
             IDictionary<string, object> userProperties,
             string messageId)
         {
         }
-
-        //[FunctionName("Timer")]
-        //public void Accounts([TimerTrigger("* * * * * *")]TimerInfo myTimer)
-        //{
-        //}
     }
 }
